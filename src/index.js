01603cbfd089b44efb3d57b7bd02d510d0927eef
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-09-15 00:27:16 +0200

    Add starter files

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import { log } from 'console'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')) // 'combined' outputs the Apache style LOGs
+}
+
+const PORT = process.env.PORT || 1337
+
+app.get('/', (req, res) => {
+    res.json({ hello: 'world' })
+})
+
+// Add routes for 404 and error handling
+// Catch 404 and forward to error handler
+// Put this last
+app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+app.use(({ status, message: title }, req, res, next) => {
+    if (res.headersSent) {
+        return next(err)
+    }
+
+    res.status(status || 500).json({
+        errors: [
+            {
+                status,
+                title,
+            },
+        ],
+    })
+})
+
+app.listen(PORT, () => {
+    log(`server listening on ${PORT}`)
+})
