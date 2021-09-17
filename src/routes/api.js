41ae3323f2cd02c404356fe73bf3d842b4e9b2b5
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-09-17 14:41:35 +0200

    Reuse functions for responses, add more error information

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,37 +2,43 @@ import express from 'express'
 import database from '../db/database.js'
 const router = express.Router()
 
+const returnError = (res, errors) =>
+    res.status(500).json({ errors: { message: errors.message, errors } })
+
+const returnSuccess = (res, result, status) =>
+    res.status(status).json({ data: result })
+
 router
-    .post('/create', async ({ body: { html, name } }, res) => {
+    .post('/create', async ({ body: { html, name } }, res, _next) => {
         try {
             const result = await database.insertHTML(name, html)
-            return res.status(201).json({ data: result })
+            return returnSuccess(res, result, 201)
         } catch (errors) {
-            return res.status(500).json({ errors: errors.message })
+            return returnError(res, errors)
         }
     })
-    .put('/update', async ({ body: { id, html, name } }, res, next) => {
+    .put('/update', async ({ body: { id, html, name } }, res, _next) => {
         try {
             const result = await database.updateDocument(id, html, name)
-            return res.status(200).json({ data: result })
+            return returnSuccess(res, result, 200)
         } catch (errors) {
-            return res.status(500).json({ errors: errors.message })
+            return returnError(res, errors)
         }
     })
-    .get('/all', async (req, res, next) => {
+    .get('/', async (_req, res, _next) => {
         try {
             const result = await database.getAllHTMLDocuments()
-            return res.status(200).json({ data: result })
+            return returnSuccess(res, result, 200)
         } catch (errors) {
-            return res.status(500).json({ errors: errors.message })
+            return returnError(res, errors)
         }
     })
-    .get('/:id', async ({ params: { id } }, res, next) => {
+    .get('/:id', async ({ params: { id } }, res, _next) => {
         try {
             const result = await database.getOneDocument(id)
-            return res.status(200).json({ data: result })
+            return returnSuccess(res, result, 200)
         } catch (errors) {
-            return res.status(500).json({ errors: errors.message })
+            return returnError(res, errors)
         }
     })
 
