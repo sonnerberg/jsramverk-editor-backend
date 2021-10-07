b2222bcb6284e5f71ffefa60c1a4f6c93e679b95
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-10-07 09:55:42 +0200

    Add scipt to update .env on Azure

---
+++ update_env_on_azure.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+PASS_USERNAME_LOCATION="services/education/bth/dbwebb.se/jsramverk/editor/azure/ftps/username"
+PASS_PASSWORD_LOCATION="services/education/bth/dbwebb.se/jsramverk/editor/azure/ftps/password"
+PASS_URL_LOCATION="services/education/bth/dbwebb.se/jsramverk/editor/azure/ftps/url"
+lftp -u \
+  "$(pass "$PASS_USERNAME_LOCATION")","$(pass "$PASS_PASSWORD_LOCATION")" \
+  "$(pass "$PASS_URL_LOCATION")" << EOF
+
+put .env
+
+bye
+EOF
