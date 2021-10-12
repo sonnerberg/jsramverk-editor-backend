cb20eef5042d5bb9af786c8829dd8eacc38b36ca
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-10-12 11:17:16 +0200

    Add database schema

---
+++ src/db/schema.js
@@ -0,0 +1,22 @@
+export const schema = {
+    validator: {
+        $jsonSchema: {
+            bsonType: 'object',
+            additionalProperties: false,
+            required: ['name', 'html'],
+            properties: {
+                _id: {
+                    bsonType: 'objectId',
+                },
+                html: {
+                    bsonType: 'string',
+                    description: 'must be a string and is required',
+                },
+                name: {
+                    bsonType: 'string',
+                    description: 'must be a string and is required',
+                },
+            },
+        },
+    },
+}
