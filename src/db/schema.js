82d4a1e0ba2b7a2ac951a0e1f53b0550bfd1ec79
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-10-20 23:43:21 +0200

    Update database schema (add minLength)

--- src/db/schema.js
+++ src/db/schema.js
@@ -11,10 +11,12 @@ export const schema = {
                 html: {
                     bsonType: 'string',
                     description: 'must be a string and is required',
+                    minLength: 1
                 },
                 name: {
                     bsonType: 'string',
                     description: 'must be a string and is required',
+                    minLength: 1
                 },
             },
         },
