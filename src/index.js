07820524640f33beada8d23c2b8b56e3fcdf34c1
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-10-26 09:33:03 +0200

    Use only bth as origin

--- src/index.js
+++ src/index.js
@@ -22,13 +22,13 @@ const io = new Server(httpServer, {
     cors: {
         origin:
             process.env.NODE_ENV === 'production'
-                ? [
-                      'www.student.bth.se',
-                      'https://jsramverk-editor-pene14.azurewebsites.net/',
-                  ]
-                : 'http://localhost:3000',
+                ? // [
+                  'https://www.student.bth.se'
+                : //   'https://jsramverk-editor-pene14.azurewebsites.net/',
+                  //   ]
+                  'http://localhost:3000',
         methods: ['GET', 'POST'],
-        allowedHeaders: ['Access-Control-Allow-Origin'],
+        // allowedHeaders: ['Access-Control-Allow-Origin'],
     },
 })
 
