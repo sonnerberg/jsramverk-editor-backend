bf3aac01089dffd3fd98f05f544baa78d5f1b697
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-12-12 22:07:42 +0100

    Try using https

--- src/getFrontendURL.js
+++ src/getFrontendURL.js
@@ -2,6 +2,6 @@ const { NODE_ENV } = process.env
 
 export function getFrontendURL() {
     return NODE_ENV === 'production'
-        ? 'http://www.student.bth.se'
+        ? 'https://www.student.bth.se'
         : 'http://localhost:3000'
 }
