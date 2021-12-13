301482cdb60f2d981b6e05967a87a290577d1906
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2021-12-13 21:15:18 +0100

    Fix logging in with Github

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -14,7 +14,7 @@ authRouter
         }),
         function (_req, res) {
             // Successful authentication, redirect home.
-            res.redirect(`${getFrontendURL()}/editor`)
+            res.redirect(`${getFrontendURL()}/~pene14/editor`)
         }
     )
     .get(
